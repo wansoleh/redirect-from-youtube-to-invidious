d3109737eb3bc6928a464977c881a9692089392f
wansoleh <108913757+wansoleh@users.noreply.github.com>
2024-08-06 02:30:20 +0700

    make script more compact

--- youtube-redirect-to-piped.user.js
+++ youtube-redirect-to-piped.user.js
@@ -9,22 +9,15 @@
 // @match        *://m.youtube.com/*
 // @grant        none
 // ==/UserScript==
-
 (function() {
     'use strict';
-
     const currentUrl = new URL(window.location.href);
     const baseUrl = 'https://piped.video';
-
     let newUrl = baseUrl;
-
     if (currentUrl.hostname === 'youtu.be') {
         newUrl += `/${currentUrl.pathname.substring(1)}`;
     } else if (['www.youtube.com', 'youtube.com', 'm.youtube.com'].includes(currentUrl.hostname)) {
         newUrl += currentUrl.pathname === '/' ? '/' : `${currentUrl.pathname}${currentUrl.search}`;
-    } else {
-        newUrl += '/trending';
-    }
-
+    } 
     window.location.href = newUrl;
 })();
